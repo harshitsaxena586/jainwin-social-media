3c817199de7510066faaba40db225dc4aaf4974d
harshit saxena <harshitsaxena586@gmail.com>
2021-06-16 20:49:03 +0530

    clearing up the branch

--- src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Posts from "../features/posts/Posts";
-export default function Home() {
-  return (
-    <div className=" ">
-      <Posts />
-    </div>
-  );
-}
--- src/pages/ProfilePage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Profile from "../features/profile/Profile";
-export default function ProfilePage() {
-  return (
-    <div>
-      <Profile />
-    </div>
-  );
-}
